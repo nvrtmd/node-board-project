d53c3ed7c1c691f8dd091af8d5ab096e5d1fdaed
YUZAMIN <untmd.crmn@gmail.com>
2022-08-24 18:27:15 +0900

    :sparkles: create post creating(post) method

--- routes/board.js
+++ routes/board.js
@@ -8,4 +8,18 @@ router.get("/list", async (req, res, next) => {
   res.json(postsData);
 });
 
+router.post("/create", async (req, res) => {
+  const postData = {
+    post_title: req.body.postTitle,
+    post_contents: req.body.postContents,
+    post_views: 0,
+    post_display: JSON.parse(req.body.postDisplay),
+    post_register_date: Date.now(),
+    post_register_user_name: "anonymous",
+  };
+
+  await Post.create(postData);
+  res.sendStatus(201);
+});
+
 module.exports = router;
