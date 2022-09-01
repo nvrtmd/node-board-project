7b7a63134a3383fca78bb368ecc9a964441cea58
YUZAMIN <untmd.crmn@gmail.com>
2022-09-02 03:02:59 +0900

    :sparkles: add origin option to access resources for user authentication

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   cors({
-    origin: true,
-    credential: true,
+    credentials: true,
+    origin: "https://react-board-project.netlify.app/",
   })
 );
 
