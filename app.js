6b534967135177d9c7b98ca8308da619225a58d1
YUZAMIN <untmd.crmn@gmail.com>
2022-09-02 02:27:43 +0900

    :sparkles: add options to access resources for user authentication

--- app.js
+++ app.js
@@ -22,7 +22,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credential: true,
+  })
+);
 
 app.use("/board", boardRouter);
 app.use("/user", userRouter);
