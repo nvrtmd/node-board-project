db0159e82e86455177acf9e5195382dabdccd67a
YUZAMIN <untmd.crmn@gmail.com>
2022-09-01 17:32:21 +0900

    :heavy_plus_sign: install cors package

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+import cors from "cors";
 const sequelize = require("./models/index.js").sequelize;
 require("dotenv").config();
 
@@ -21,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/board", boardRouter);
 app.use("/user", userRouter);
