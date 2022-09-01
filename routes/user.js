cc30b12a34da66a265fe2104d93f98d3a9a6024f
YUZAMIN <untmd.crmn@gmail.com>
2022-09-02 04:53:30 +0900

    :bug: replace writeHeader with setHeader

--- routes/user.js
+++ routes/user.js
@@ -78,7 +78,8 @@ router.post(
 
     // res.cookie("token", token, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
 
-    res.writeHead(201, { "Set-Cookie": `token=${token}; HttpOnly` });
+    res.setHeader(201, { "Set-Cookie": `token=${token}; HttpOnly` });
+
     res.status(201).json({
       code: 201,
       message: "created successfully.",
