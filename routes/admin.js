47372cc82ae051a45bca325c610cfe1b03a1b456
YUZAMIN <untmd.crmn@gmail.com>
2022-09-18 15:10:16 +0900

    :sparkles: create user data get method

--- routes/admin.js
+++ routes/admin.js
@@ -12,13 +12,26 @@ const {
 const { User } = require("../models/index");
 
 /**
- * 회원 정보 조회
+ * 회원 목록 조회
  */
 router.get("/userlist", isAdminUser, async (req, res, next) => {
   const usersData = await User.findAll();
   return res.status(200).json({
     code: 200,
     data: usersData,
+  });
+});
+
+/**
+ * 단일 회원 정보 조회
+ */
+router.get("/user/:userIndex", isAdminUser, async (req, res, next) => {
+  const userIndex = req.params.userIndex;
+
+  const userData = await User.findOne({ where: { id: userIndex } });
+  return res.status(200).json({
+    code: 200,
+    data: userData,
   });
 });
 
