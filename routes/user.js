a0e5a32b1273da8aa3f5d1d6c7ed8e23b645b4a5
YUZAMIN <untmd.crmn@gmail.com>
2022-09-23 18:39:35 +0900

    :sparkles: apply user controller modules to user routers

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const jwt = require("jsonwebtoken");
 const fs = require("fs");
 const multer = require("multer");
 const userMiddlewares = require("../middlewares/userMiddlewares");
+const userControllers = require("../controllers/userControllers");
 
 const { User } = require("../models/index");
 
@@ -20,27 +21,7 @@ const { User } = require("../models/index");
 /**
  * 회원가입
  */
-router.post(
-  "/signup",
-  userMiddlewares.isValidSignup,
-  async (req, res, next) => {
-    const encodedPassword = await bcrypt.hash(req.body.userPassword, 12);
-    const userData = {
-      user_id: req.body.userId,
-      user_nickname: req.body.userNickname,
-      user_password: encodedPassword,
-      user_name: req.body.userName,
-      user_phone: req.body.userPhone,
-      // user_profile_image: req.body.userProfileImage,
-    };
-    await User.create(userData);
-
-    return res.status(201).json({
-      code: 201,
-      message: "created successfully.",
-    });
-  }
-);
+router.post("/signup", userMiddlewares.isValidSignup, userControllers.signup);
 
 /**
  * (개발용) 회원가입 페이지
@@ -58,46 +39,13 @@ router.post(
   userMiddlewares.isValidSignin,
   userMiddlewares.isExistedId,
   userMiddlewares.isCorrectPassword,
-  async (req, res, next) => {
-    const userData = {
-      userId: req.body.userId,
-    };
-
-    const token = jwt.sign(userData, process.env.JWT_SECRET_KEY, {
-      expiresIn: process.env.JWT_EXPIRE_TIME,
-      issuer: process.env.JWT_ISSUER,
-    });
-
-    res.setHeader(
-      "Set-Cookie",
-      `token=${token}; Path=/; HttpOnly; SameSite=none; secure=true;`
-    );
-
-    res.status(201).json({
-      code: 201,
-      message: "created successfully.",
-    });
-  }
+  userControllers.signin
 );
 
 /**
  * 로그아웃
  */
-router.get("/signout", userMiddlewares.isSignedIn, async (req, res, next) => {
-  const token = req.headers.cookie.split("=")[1];
-
-  res.setHeader(
-    "Set-Cookie",
-    `token=${token}; Path=/; HttpOnly; SameSite=none; secure=true; Max-Age=0`
-  );
-
-  // res.cookie("token", token, cookieConfig);
-
-  return res.status(200).json({
-    code: 200,
-    message: "signed out successfully.",
-  });
-});
+router.get("/signout", userMiddlewares.isSignedIn, userControllers.signout);
 
 /**
  * 회원 탈퇴
@@ -105,91 +53,21 @@ router.get("/signout", userMiddlewares.isSignedIn, async (req, res, next) => {
 router.delete(
   "/deleteuser",
   userMiddlewares.isSignedIn,
-  async (req, res, next) => {
-    const token = req.headers.cookie.split("=")[1];
-    const signedInUserId = jwt.verify(token, process.env.JWT_SECRET_KEY).userId;
-
-    const deletedUserData = await User.findOne({
-      where: { user_id: signedInUserId },
-    });
-
-    if (deletedUserData) {
-      await User.destroy({ where: { user_id: signedInUserId } });
-      const token = req.headers.cookie.split("=")[1];
-      res.setHeader(
-        "Set-Cookie",
-        `token=${token}; Path=/; HttpOnly; SameSite=none; secure=true; Max-Age=0`
-      );
-
-      // res.cookie("token", token, cookieConfig);
-
-      return res.status(200).json({
-        code: 200,
-        message: "deleted successfully.",
-      });
-    } else {
-      return res.status(404).json({
-        code: 404,
-        message: "cannot find user.",
-      });
-    }
-  }
+  userControllers.deleteUser
 );
 
 /**
  * 회원 정보 조회
  */
-router.get("/profile", userMiddlewares.isSignedIn, async (req, res, next) => {
-  const token = req.headers.cookie.split("=")[1];
-  const signedInUserId = jwt.verify(token, process.env.JWT_SECRET_KEY).userId;
-
-  const userData = await User.findOne({
-    where: { user_id: signedInUserId },
-  });
-
-  return res.status(200).json({
-    code: 200,
-    data: userData,
-  });
-});
+router.get("/profile", userMiddlewares.isSignedIn, userControllers.getProfile);
 
 /**
  * 회원 정보 수정
  */
-router.post("/profile", userMiddlewares.isSignedIn, async (req, res, next) => {
-  const token = req.headers.cookie.split("=")[1];
-  const signedInUserId = jwt.verify(token, process.env.JWT_SECRET_KEY).userId;
-  const encodedPassword = await bcrypt.hash(req.body.userPassword, 12);
-
-  const modifyUserData = {
-    user_id: req.body.userId,
-    user_nickname: req.body.userNickname,
-    user_password: encodedPassword,
-    user_name: req.body.userName,
-    user_phone: req.body.userPhone,
-  };
-
-  await User.update(modifyUserData, { where: { user_id: signedInUserId } });
-
-  const newUserData = {
-    userId: modifyUserData.user_id,
-  };
-  const newToken = jwt.sign(newUserData, process.env.JWT_SECRET_KEY, {
-    expiresIn: process.env.JWT_EXPIRE_TIME,
-    issuer: process.env.JWT_ISSUER,
-  });
-
-  res.setHeader(
-    "Set-Cookie",
-    `token=${newToken}; Path=/; HttpOnly; SameSite=none; secure=true;`
-  );
-
-  // res.cookie("token", token, cookieConfig);
-
-  return res.status(201).json({
-    code: 201,
-    message: "modified successfully.",
-  });
-});
+router.post(
+  "/profile",
+  userMiddlewares.isSignedIn,
+  userControllers.modifyProfile
+);
 
 module.exports = router;
